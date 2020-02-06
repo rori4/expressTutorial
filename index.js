c215dc9ad9a1ce2319724a82387ee6025beae8fd
rori4 <rangelstoilov@gmail.com>
2020-02-06 22:31:54 +0200

    Middleware tutorial

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express')
 let app = express()
 const port = 3000
 
+const users = [
+	{name: 'Peter', age: 24},
+	{name: 'John', age: 50},
+]
 
-app.route('/redirectMe')
-	.get((req, res) => {
-		res.redirect('/redirected')
-	})
-
-app.get('/redirected', (req, res) => {
-	res.send('You have been redirected')
+app.get('/', (req, res, next) => {
+	req.user = users.find(u => u.name === 'Peter')
+	next()
+}, (req, res) => {
+	const user = req.user || {}
+	res.send(`Welcome Home,${user.name}`)
 })
 
 
 app.listen(port, () => console.log(`Express is running on port ${port}`))
-
